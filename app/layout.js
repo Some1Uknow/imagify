15ee4bbdf89529bed09a70ff427a6e93804a9c3a
Some1Uknow <raghu250407@gmail.com>
2024-07-24 00:17:18 +0530

    Added cloudflare analytics

--- app/layout.js
+++ app/layout.js
@@ -6,13 +6,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Imagify",
   description: "Text to Image",
-  keywords: ['Text to image','AI image', 'AI Art', 'Generate Image']
+  keywords: ["Text to image", "AI image", "AI Art", "Generate Image"],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "ca434bedbcbd4519b38644b222f93aee"}'
+        ></script>
+      </body>
     </html>
   );
 }
